d209440815cf388d7eccf060ceb0634d17d90e1f
Rod Leonard <leonard@cec.sc.edu>
2020-01-12 14:53:44 -0500

    removed debugging messages from FindQwt

--- _CMakeModules/FindQwt.cmake
+++ _CMakeModules/FindQwt.cmake
@@ -117,11 +117,6 @@ mark_as_advanced (
   QWT_ROOT_DIR
 )
 
-message(STATUS "Include: " ${QWT_INCLUDE_DIRS})
-message(STATUS "Libraries: " ${QWT_LIBRARIES})
-message(STATUS "ImplibDebug: " ${QWT_LIBRARY_DEBUG})
-message(STATUS "Implib: " ${QWT_LIBRARY})
-
 if(QWT_FOUND AND NOT TARGET Qwt::Qwt)
     add_library(Qwt::Qwt SHARED IMPORTED GLOBAL)
     set_target_properties(Qwt::Qwt PROPERTIES
@@ -129,6 +124,6 @@ if(QWT_FOUND AND NOT TARGET Qwt::Qwt)
         INTERFACE_LINK_LIBRARIES ${QWT_LIBRARIES}
         IMPORTED_IMPLIB_DEBUG ${QWT_LIBRARY_DEBUG}
         IMPORTED_IMPLIB ${QWT_LIBRARY}
-	IMPORTED_LOCATION ${QWT_LIBRARY})
+	      IMPORTED_LOCATION ${QWT_LIBRARY})
     set_property(TARGET Qwt::Qwt APPEND PROPERTY INTERFACE_LINK_LIBRARIES Qt5::Widgets)
 endif()
